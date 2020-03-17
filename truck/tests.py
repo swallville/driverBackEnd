bb03f13aa68009c8720d92fbb72da88e97bd1e6d
lukasferreira <unlisislukasferreira@hotmail.com>
2020-03-17 08:24:49 -0300

    chore(app): finishing truck app tests

--- truck/tests.py
+++ truck/tests.py
@@ -1,3 +1,114 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from django.urls import reverse
+
+from truck.models import Truck
+
 
 # Create your tests here.
+class TruckTests(APITestCase):
+    data = {'truck_type': '1'}
+
+    def test_truck_create(self):
+        data = self.data.copy()
+
+        response = self.client.post(reverse(
+            'truck:truck_create'), data,
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Truck.objects.count(), 1)
+
+    def test_truck_update(self):
+        data = self.data.copy()
+
+        response = self.client.post(reverse(
+            'truck:truck_create'), data,
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Truck.objects.count(), 1)
+
+        data['truck_type'] = '2'
+
+        response = self.client.put(reverse(
+            'truck:truck_update_retrieve_delete',
+            kwargs={'pk': Truck.objects.last().id}
+        ), data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('truck_type'), '2')
+        self.assertEqual(Truck.objects.count(), 1)
+
+    def test_truck_retrieve(self):
+        data = self.data.copy()
+
+        response = self.client.post(reverse(
+            'truck:truck_create'), data,
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Truck.objects.count(), 1)
+
+        response = self.client.get(reverse(
+            'truck:truck_update_retrieve_delete',
+            kwargs={'pk': Truck.objects.last().id}
+        ), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('truck_type'), '1')
+        self.assertEqual(Truck.objects.count(), 1)
+
+    def test_truck_delete(self):
+        data = self.data.copy()
+
+        response = self.client.post(reverse(
+            'truck:truck_create'), data,
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Truck.objects.count(), 1)
+
+        response = self.client.delete(reverse(
+            'truck:truck_update_retrieve_delete',
+            kwargs={'pk': Truck.objects.last().id}
+        ), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Truck.objects.count(), 0)
+
+    def test_truck_list(self):
+        data = self.data.copy()
+
+        response = self.client.post(reverse(
+            'truck:truck_create'), data,
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Truck.objects.count(), 1)
+
+        response = self.client.get(reverse(
+            'truck:truck_list'
+        ), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('count'), 1)
+        self.assertEqual(Truck.objects.count(), 1)
+
+        data = self.data.copy()
+
+        response = self.client.post(reverse(
+            'truck:truck_create'), data,
+            format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Truck.objects.count(), 2)
+
+        response = self.client.get(reverse(
+            'truck:truck_list'
+        ), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('count'), 2)
+        self.assertEqual(Truck.objects.count(), 2)
--- truck/urls.py
+++ truck/urls.py
@@ -5,12 +5,12 @@ from truck.views import TruckCreateView, TruckUpdateRetrieveDeleteView, TruckLis
 app_name = 'truck'
 
 urlpatterns = [
-    url(r'(?P<pk>\d+)/update_retrieve_delete',
-        TruckUpdateRetrieveDeleteView.as_view({'put': 'update', 'get': 'retrieve', 'delete': 'destroy'}),
-        name='truck_update_retrieve_delete'),
     url(r'create',
         TruckCreateView.as_view({'post': 'create'}),
         name='truck_create'),
+    url(r'(?P<pk>\d+)/update_retrieve_delete',
+        TruckUpdateRetrieveDeleteView.as_view({'put': 'update', 'get': 'retrieve', 'delete': 'destroy'}),
+        name='truck_update_retrieve_delete'),
     url(r'list',
         TruckListView.as_view({'get': 'list'}),
         name='truck_list'),
